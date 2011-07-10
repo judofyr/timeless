893ae498dbd9efc494d5bf828d8473a5d8493846
Magnus Holm <judofyr@gmail.com>
2011-07-10 12:12:40 +0200

    Sort the index alphabetically

--- timeless/models/entry.rb
+++ timeless/models/entry.rb
@@ -7,7 +7,7 @@ module Timeless::Models
     end
 
     def self.entries
-      Dir["content/*"].reject { |f| f =~ /~/ }.map { |f| File.basename(f) }
+      Dir["content/*"].reject { |f| f =~ /~/ }.map { |f| File.basename(f) }.sort
     end
 
     def self.new(name, force = false)
