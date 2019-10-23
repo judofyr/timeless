a9097fe332b491ef0f92488af9419e2ff979a484
Magnus Holm <judofyr@gmail.com>
2019-10-23 08:21:27 +0200

    Add support for warning note on stale content.

--- app/adoc_page.rb
+++ app/adoc_page.rb
@@ -33,6 +33,27 @@ class AdocPage < Page
     @content ||= doc.render
   end
 
+  def review_date
+    @review_date ||=
+      if str = doc.attributes["review-date"]
+        Date.parse(str)
+      end
+  end
+
+  def stale_content?
+    !!doc.attributes["review-stale"]
+  end
+
+  OUT_OF_DATE = 200
+
+  def stale_review?
+    if rd = review_date
+      Date.today - rd > OUT_OF_DATE
+    else
+      true
+    end
+  end
+
   def to_tubby
     Tubby.new { |t|
       t.article(class: "post") {
@@ -41,8 +62,33 @@ class AdocPage < Page
           t.p {
             t << "Written by "
             t << doc.author
+            t << ". "
+
+            if rd = review_date
+              t << "Last updated "
+              t << rd.strftime("%B %-d, %Y")
+              t << ". "
+            end
           }
         }
+
+        if stale_content?
+          t.div(class: "alert error") {
+            t.p {
+              t.strong("Note: ")
+              t << "The following content is known to be out of date. "
+              t << "You might still find it useful, but be aware. "
+            }
+          }
+        elsif stale_review?
+          t.div(class: "alert warning") {
+            t.p {
+              t.strong("Note: ")
+              t << "The following content hasn't been reviewed in a while and it might be out of date. "
+            }
+          }
+        end
+
         t.raw!(content)
       }
     }
